484551d0444b15401f746d59b291f22add430d50
Lola <lola@example.com>
2022-11-11 19:18:00 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите количество элементов Вашего массива: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+string[] Array1 = new string[n]; 
+
+for (int i = 0; i < n; i++)
+    {
+    Console.Write("Элемент №{0}:   ", i + 1);
+    Array1[i] = Console.ReadLine();
+    }
+
+string[] array2 = new string[Array1.Length];
+int count = 0;
+
+for (int i = 0; i < Array1.Length; i++)
+    {
+    if(Array1[i].Length <= 3)
+        {
+        array2[count] = Array1[i];
+        count++;
+        }
+    }
+
+void PrintArray(string[] Array1)
+{
+    for (int i = 0; i < Array1.Length; i++)
+    {
+        Console.Write($"{Array1[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array2);
